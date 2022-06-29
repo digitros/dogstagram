5b11b873ae99cb99ca6050b3c6a26008d38f20bb
Diego Valdez <dgogo9@hotmail.com>
2022-06-28 20:50:50 -0500

    Initial Svelte configuration

---
+++ src/containers/App.svelte
@@ -0,0 +1,14 @@
+<style>
+  :global(body) {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    background-color: #637383;
+    color: #fff;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello Dogstagram</h1>
+</div>
