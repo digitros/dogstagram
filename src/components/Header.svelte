38e0448fb447424b788d80600bb948b7fd92c480
Diego Valdez <dgogo9@hotmail.com>
2022-07-05 14:44:59 -0500

    Added Header markup

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,19 @@
-<div class="Header">Header</div>
+<div class="Header">
+  <div class="Header-container">
+    <div class="Header-content">
+      <div class="Header-logo">
+        <h1>Dogstagram</h1>
+      </div>
+      <div class="Header-nav">
+        <ul>
+          <li>
+            <i class="fa-solid fa-heart" />
+          </li>
+          <li>
+            <i class="fa-solid fa-user-alt" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
